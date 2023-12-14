4acd27baced40d5de0cb610f2b86935bf26ffa37
nilbarbosa <149343130+nilbarbosa@users.noreply.github.com>
2023-12-13 21:32:33 -0300

    criei pasta da aula 11 e finalizei os exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var  vel = 78
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'FR'
+console.log(`vivendo em ${pais}`)
+if (pais == 'BR') {
+    console.log(`você é Brasileiro`)
+} else {
+    console.log(`você é estrangeiro`)
+}
